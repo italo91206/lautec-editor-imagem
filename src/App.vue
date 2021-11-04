eab85ed880418c4739a87cdfc3c3544b9d60692a
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-11-04 20:26:55 -0300

    image load and position handler

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form-group">
+      <label>Capturar imagem</label>
+      <input type="file" accept="image/*" capture @input="imagemInput">
+    </div>
+
+    <div class="form-group">
+      <label for="">Abrir galeria</label>
+      <input type="file" accept="image/*" @input="imagemInput">
+    </div>
+
+    <div id="konva-stage"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Konva from 'konva';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      data: null,
+      image_url: null,
+      stage: null,
+      layer: null,
+    }
+  },
+  methods: {
+    imagemAdicionada(){
+      let stage = this.stage;
+      stage.on('click', function(){
+        var mousePos = stage.getPointerPosition();
+        console.log(mousePos);
+      })
+      
+    },
+    adicionarImagemNoLayer(imagem_objeto){
+      let imagem = new Konva.Image({
+        image: imagem_objeto,
+        id: 'imagem'
+      })
+
+      this.layer = new Konva.Layer();
+      this.layer.add(imagem);
+      this.stage.add(this.layer);
+      this.imagemAdicionada();
+    },
+    imagemInput(e){
+      let imagem_input = e.target.files[0];
+      this.image_url = URL.createObjectURL(imagem_input);
+      
+      let imagem_objeto = new Image();
+      imagem_objeto.src = this.image_url;
+      imagem_objeto.onload = this.adicionarImagemNoLayer(imagem_objeto);
+    },
+  },
+  mounted(){
+    var width = 500;
+    var height = 500;
+
+    this.stage = new Konva.Stage({
+      container: 'konva-stage',
+      width: width,
+      height: height,
+    });
   }
 }
 </script>
