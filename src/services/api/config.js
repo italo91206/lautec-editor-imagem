1a5efb49caf093363f927d80cade774187a5e14c
italo91206 <italoferreiradjk14@gmail.com>
2022-03-21 18:48:57 -0300

    added network address for mobile local

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -2,7 +2,8 @@ const ENVIROMENT = process.env.NODE_ENV;
 
 export const config = {
     setBaseUrl: () => {
-        let baseURL = `http://localhost:3000/`
+        let baseURL = `http://192.168.0.131:3000/`
+        // let baseURL = `http://localhost:3000/`
 
         if(ENVIROMENT === 'production') baseURL = `https://lautech-backend.herokuapp.com/`
 
